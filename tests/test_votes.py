3ec95657c0f7ebf0cd72bc713342137f74ceaae6
Kamlesh <kamlesh_fw11_179@masai.school>
2022-03-05 12:03:52 +0530

    some update in ci/cd

---
+++ tests/test_votes.py
@@ -0,0 +1,37 @@
+from pytest import fixture
+from app import models
+
+
+import pytest
+from app import models
+
+@pytest.fixture()
+def test_vote(test_post,session,test_user):
+    test_vote = models.Vote(post_id=test_post[3].id,user_id=test_user["id"])
+
+def test_votes(authorized_client,test_post,test_user2):
+   res = authorized_client.post("/vote/",json={"post_id":test_post[3].id,"dir": 1})
+   assert res.status_code == 201
+
+
+def test_votes_twice(authorized_client,test_post,test_vote):
+    res = authorized_client.post("/vote/",json={"post_id":test_post[3].id,"dir":1})
+    # assert res.status_code == 409
+    assert res.status_code == 201
+
+def test_delete_a_vote(authorized_client,test_post,test_vote):
+    res = authorized_client.post("/vote/",json={"post_id":test_post[3].id,"dir":0})
+    assert res.status_code == 201
+
+def test_delete_vote_non_exsist(authorized_client,test_post,test_vote):
+     res = authorized_client.post("/vote/",json={"post_id":test_post[3].id,"dir":0})
+     assert res.status_code == 404
+    
+
+def test_delete_vote_non_exsist(authorized_client,test_post,test_vote):
+     res = authorized_client.post("/vote/",json={"post_id":80000,"dir":1})
+     assert res.status_code == 404
+
+def test_vote_on_unauthorized_user(client,test_post,test_vote):
+     res = client.post("/vote/",json={"post_id":80000,"dir":1})
+     assert res.status_code == 404
